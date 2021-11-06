f8444cc9edf146586b0325f0fc229319dfc4196e
BOB450 <bwvoskuil@gmail.com>
2021-11-05 23:59:50 -0600

    Update main.java
    added onEnable function

--- Plugin/src/LinkerPkg/main.java
+++ Plugin/src/LinkerPkg/main.java
@@ -1,8 +1,15 @@
 package LinkerPkg;
 
+import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class main extends JavaPlugin implements Listener {
+    @Override
+    public void onEnable()
+    {
+        Bukkit.getPluginManager().registerEvents(this,this);
+        getLogger().info("LINKER PLUGIN HAS BEEN ENABLED! by BOB450");
 
+    }
 }
