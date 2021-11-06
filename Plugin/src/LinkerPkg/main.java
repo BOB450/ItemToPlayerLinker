e932fbbdf5e99b85ab798bb0cefcf74280e57f48
BOB450 <bwvoskuil@gmail.com>
2021-11-06 11:29:27 -0600

    Update main.java
    added a block beak function

--- Plugin/src/LinkerPkg/main.java
+++ Plugin/src/LinkerPkg/main.java
@@ -1,15 +1,27 @@
 package LinkerPkg;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class main extends JavaPlugin implements Listener {
+
+    public void doBreakStuff (Block block)
+    {
+        Bukkit.broadcastMessage("The blocks type is" + block.getType());
+    }
+
+
     @Override
     public void onEnable()
     {
@@ -24,10 +36,26 @@ public class main extends JavaPlugin implements Listener {
             for (ItemStack is : p.getInventory()){
                 if (is.getType() == Material.DIRT){
                     p.sendMessage("You have dirt");
+
                 }
             }
 
         }
         return true;
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event)
+    {
+        Player plr = event.getPlayer();
+        event.setJoinMessage(ChatColor.AQUA + "Welcome blank server " + plr.getDisplayName() + " use the /helpsmp command to get more info " );
+        //Bukkit.broadcastMessage(plr + "Has joined the serve ");
+
+    }
+
+    public void onBlockBreak(BlockBreakEvent event)
+    {
+        doBreakStuff(event.getBlock());
+        //Your Code Here
     }
 }
