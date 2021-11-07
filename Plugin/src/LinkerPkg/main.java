cc5b4027488669504cb0bfc6addf796ae718643d
BOB450 <bwvoskuil@gmail.com>
2021-11-06 21:24:29 -0600

    Update main.java
    add new costom item funtion that is called on player join

--- Plugin/src/LinkerPkg/main.java
+++ Plugin/src/LinkerPkg/main.java
@@ -2,23 +2,48 @@ package LinkerPkg;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class main extends JavaPlugin implements Listener {
 
+
+    public static ItemStack wand;
+
+
+    private static void createWand(Player PL) {
+        ItemStack item = new ItemStack(Material.DRAGON_EGG, 1);
+        ItemMeta meta = item.getItemMeta();
+        meta.setDisplayName("");
+        List<String> lore = new ArrayList<>();
+        lore.add("§7This is your Egg keep it SAFE!");
+        meta.setLore(lore);
+       // meta.addEnchant(Enchantment.LUCK, 1, false);
+       // meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+        item.setItemMeta(meta);
+        wand = item;
+    }
+
     public void doBreakStuff (Block block)
     {
-        Bukkit.broadcastMessage("The blocks type is" + block.getType());
+
+        Bukkit.broadcastMessage("The blocks type is" + block);
     }
 
 
@@ -48,14 +73,29 @@ public class main extends JavaPlugin implements Listener {
     public void onPlayerJoin(PlayerJoinEvent event)
     {
         Player plr = event.getPlayer();
+        createWand(plr);
+
         event.setJoinMessage(ChatColor.AQUA + "Welcome blank server " + plr.getDisplayName() + " use the /helpsmp command to get more info " );
+        if(plr.hasPlayedBefore() == true) {
+            ItemStack i  = new ItemStack(Material.DRAGON_EGG);
+           i.getItemMeta().setDisplayName("Costom egg");
+            plr.getInventory().addItem(wand);
+            for (ItemStack is : plr.getInventory()){
+                if (is.getType() == Material.DRAGON_EGG){
+                    plr.sendMessage("You have a dragon egg");
+                }
+            }
+        }
+
         //Bukkit.broadcastMessage(plr + "Has joined the serve ");
 
     }
-
+    @EventHandler
     public void onBlockBreak(BlockBreakEvent event)
     {
         doBreakStuff(event.getBlock());
         //Your Code Here
     }
+
+
 }
